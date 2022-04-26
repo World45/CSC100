4cbc04d59710615f232631972493dd3f56aa06ed
World45 <104409987+World45@users.noreply.github.com>
2022-04-26 00:06:04 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+#Fabio Da Silva
+#CSC_100_002
+#Final Project
+
+a_list = [65123,84453,81453,48201,48536,15689,48563,45687,98756,23654,15643,48465,45621,75641,75143,68763,25953,18946,21679,54213,46123,32652,15623,21321,105231,56423,15861,13514,87436,98465,18866,51498,56456,65453,32659,36524,54186,18412,74541,13686,41896,45964,44936,89856,15246,32168,48736,16436,65483,11483]
+
+
+
+length=len(a_list)
+sum=sum(a_list)
+average=sum/length
+maximum=max(a_list)
+minimum=min(a_list)
+
+
+
+
+
+print("The total sales across the 50 states were", sum, "dollars!")
+print ("The minimum sales were", minimum, "dollars")
+print ("The maximum sales were", maximum, "dollars")
+print ("The average sales were", average, "dollars")
